86d50f1a981cca0f07f679d7f404fd0f3fa78887
Techway <Techway@DESKTOP-OBO2JJ9>
2021-11-12 14:32:44 +0530

    user 1 first commit

---
+++ com.crm.github/src/test/java/com/crm/githubpractice/GithubPractice1Test.java
@@ -0,0 +1,12 @@
+package com.crm.githubpractice;
+
+import org.testng.annotations.Test;
+
+public class GithubPractice1Test 
+{
+	@Test
+	public void smoke()
+	{
+		System.out.println("Execute smoke test case");
+	}
+}
---
+++ com.crm.github/src/test/java/com/crm/githubpractice/GithubPractice2Test.java
@@ -0,0 +1,11 @@
+package com.crm.githubpractice;
+
+import org.testng.annotations.Test;
+
+public class GithubPractice2Test {
+	@Test
+	public void regression()
+	{
+		System.out.println("Execute regression test case");
+	}
+}
